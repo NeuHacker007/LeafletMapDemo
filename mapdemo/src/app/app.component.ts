f442dd943f3b98573bf563a3436366a4553dd17d
Ivan.Zhang <yifanivan.zhang@gmail.com>
2019-07-30 15:31:27 -0400

    -- add leaflet angular library -- set up the basic map show up

--- mapdemo/src/app/app.component.ts
+++ mapdemo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  private readonly LAYER_OSM = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 13,
+    attribution: 'Open Street Map'
+  })
+  // Values to bind to Leaflet Directive
+  layers: L.Layer[] = [];
+  layersControl = {
+    baseLayers: {
+      'Open Street Map': this.LAYER_OSM
+    },
+    overlays: {}
+  };
+  options = {
+    zoom: 13,
+    center: L.latLng(39.1696, -76.6786,)
+  };
+
+  constructor() {
+    this.apply();
+  }
+
+  apply() {
+    this.layers.push(this.LAYER_OSM);
+    return false;
+  }
 }
